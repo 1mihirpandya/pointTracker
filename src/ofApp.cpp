2928c39303cf22acc0d2eb4c82e67b9af3e56a0c
1mihirpandya <1mihirpandya@gmail.com>
2018-04-26 21:50:27 -0500

    used the elucidian theorem for color tracking to get a smoother line and provide more accuracy

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -22,6 +22,15 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    int intended_red = 0;
+    int intended_green = 200;
+    int intended_blue = 50;
+    
+    float min_difference = 255;
+    int approx_x = 0;
+    int approx_y = 0;
+    
     //vidGrabber.draw(vidGrabber.getWidth(),0,-vidGrabber.getWidth(),vidGrabber.getHeight());
     vidGrabber.draw(0,0);
     ofPixelsRef screen = vidGrabber.getPixels();
@@ -36,21 +45,35 @@ void ofApp::draw(){
     for (int i = 0; i < camWidth; i+= 1){
         for (int j = 0; j < camHeight; j+= 1){
             ofColor color = screen.getColor(i, j);
-            if (color.r <= 20 && color.g >= 100 ) {
-                addPoint(i,j);
-                point_onscreen = 0;
-                break;
+            float r_difference = color.r - intended_red;
+            float g_difference = color.g - intended_green;
+            float b_difference = color.b - intended_blue;
+            float color_difference = sqrt(r_difference * r_difference + r_difference * r_difference + r_difference * r_difference);
+            if(color_difference < min_difference){
+                min_difference = color_difference;
+                approx_x = i;
+                approx_y = j;
             }
+            //if (color.r <= 20 && color.g >= 100 ) {
+            //    addPoint(i,j);
+            //    point_onscreen = 0;
+            //    break;
+            //}
         }
     }
+    if (screen.getColor(approx_x, approx_y).r <= 20 && screen.getColor(approx_x, approx_y).g >= 100 ) {
+        addPoint(approx_x, approx_y);
+        point_onscreen = 0;
+    }
+    
     if (point_onscreen == 0)
     {
         current_line_->draw();
         point_onscreen += 1;
     } else {
         if (point_onscreen == 20) {
-        newLine();
-        point_onscreen = 21;
+            newLine();
+            point_onscreen = 21;
         } else {
             point_onscreen += 1;
         }
